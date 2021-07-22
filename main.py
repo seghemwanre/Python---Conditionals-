103d186fbfd828bc977bbded18bc74d276620b70
seghemwanre123 <86158146+seghemwanre@users.noreply.github.com>
2021-07-22 15:27:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,64 @@
+#x = 20 
+#y = 11 
+#if x > y:
+#  print('yes')
+
+#=========================
+#x = 5 
+#y = 11 
+#if x < y:
+ # print('no')
+
+#if 'hello' in 'hello world':
+ # print ('yes')
+
+#=========================
+ #x = 11
+ #y = 11 
+ #if x > y: 
+ #  print ( 'yes it is greater')
+ #elif x < y: 
+  #   print ('no it is less') 
+ #else:
+  #print ('neither')
+
+ #=========================
+
+'''
+Nested if statement 
+'''
+#num = -3
+
+#if num >= 0:
+ # if num == 0:
+  #  print ('zero')
+  #else: 
+  #  print ('positive number')
+#else:
+  #print ('negative number')
+
+#=========================
+
+'''
+One line 
+'''
+#if 7 > 5: print ('yes it is ')
+
+#=========================
+
+'''
+Tenary operator 
+'''
+#print ('yes') if 7 > 5 else print ('no')
+
+#if 7 > 5: 
+ # print ('yes')
+#else: 
+  #  print ('no')
+
+#=========================
+#if 100 > 50: 
+  #print ('yah')
+  
+
+
